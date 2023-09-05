32f68306b86b226fed1d3522b1da944c5646940c
Havish-Chowdhry <142867564+Havish-Chowdhry@users.noreply.github.com>
2023-09-05 23:49:13 +0500

    Create ex03.cpp

---
+++ Labs/03/ex03.cpp
@@ -0,0 +1,19 @@
+question ## 3 of lab manual 1
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+         int perp;
+         int base;
+         int hypotenuse;
+         printf("Enter the perp length of triangle: ");
+         scanf("%d", &perp);
+         printf("Enter the base length of triangle: ");
+         scanf("%d", &base);
+         
+         hypotenuse = sqrt((perp * perp) + (base * base));
+         printf("%d", hypotenuse);
+         return 0;
+} 
+     
