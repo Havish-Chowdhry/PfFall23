01ff51b318ae1a6815e594b74a71a9af298965c4
Havish-Chowdhry <142867564+Havish-Chowdhry@users.noreply.github.com>
2023-09-05 10:34:18 +0500

    Update question2.c

--- Labs/Lab03/question2.c
+++ Labs/Lab03/question2.c
@@ -1,1 +1,19 @@
+####Question2: Volume of a container
 
+#include <stdio.h>
+
+int main() {
+         int height = 0;
+         int length = 0;
+         int width = 0;
+         printf("\n");
+         printf("Enter length: ");
+         scanf("%d", &length);
+         printf("Enter width: ");
+         scanf("%d", &width);
+         printf("Enter height: ");
+         scanf("%d", &height);
+         int volume = length * width * height;
+         printf(" %d", volume);
+         return 0;
+}
