8995ed340546bd0dcd86532249c6abf763017927
Havish-Chowdhry <142867564+Havish-Chowdhry@users.noreply.github.com>
2023-09-05 23:46:48 +0500

    Update ex04.c

--- Labs/03/ex04.c
+++ Labs/03/ex04.c
@@ -1,1 +1,15 @@
+question ## 4 of lab manual ## 1
 
+#include <stdio.h>
+
+int main() {
+    int original_price = 1500;
+    int discount= 20;
+
+    int after_discount = (original_price*discount)/100;
+    int sale_price = (original_price - after_discount);
+    printf("The original price was: %d\n", sale_price);
+    printf("The price after discount is: %d\n", after_discount);
+    printf("The sale price is: %d\n", sale_price);
+    return 0;
+}
