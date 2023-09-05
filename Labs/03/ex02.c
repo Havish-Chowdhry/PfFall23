de49d8bee34a9fb2a65ebb970d2b2698e85d6d11
Havish-Chowdhry <142867564+Havish-Chowdhry@users.noreply.github.com>
2023-09-05 23:48:11 +0500

    Create ex02.c

---
+++ Labs/03/ex02.c
@@ -0,0 +1,19 @@
+question ## 2 of lab manual ## 1
+
+#include <stdio.h>
+
+int main() {
+         int height = 0;
+         int length = 0;
+         int width = 0;
+         printf("\n");
+         printf("Enter length: ");
+         scanf("%d", &length);
+         printf("Enter width: ");
+         scanf("%d", &width);
+         printf("Enter height: ");
+         scanf("%d", &height);
+         int volume = length * width * height;
+         printf(" %d", volume);
+         return 0;
+}
