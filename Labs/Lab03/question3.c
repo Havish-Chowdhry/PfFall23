4e54a536abc0ceae3fb1c33185ba17433704d958
Havish-Chowdhry <142867564+Havish-Chowdhry@users.noreply.github.com>
2023-09-05 10:52:35 +0500

    Update question3.c

--- Labs/Lab03/question3.c
+++ Labs/Lab03/question3.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+         int perp;
+         int base;
+         int hypotenuse;
+         printf("Enter the perp length of triangle: ");
+         scanf("%d", &perp);
+         printf("Enter the base length of triangle: ");
+         scanf("%d", &base);
+         
+         hypotenuse = sqrt((perp * perp) + (base * base));
+         printf("%d", hypotenuse);
+         return 0;
+} 
+     
