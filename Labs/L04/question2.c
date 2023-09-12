a2009bed27ef7afe1359151cb2ca7b5b709cfa80
Havish-Chowdhry <142867564+Havish-Chowdhry@users.noreply.github.com>
2023-09-12 23:30:40 +0500

    Update question2.c

--- Labs/L04/question2.c
+++ Labs/L04/question2.c
@@ -1,18 +1,22 @@
+The code prints rectangular pattern of stars of n stars
+
 #include <stdio.h>
 
 int main() {
 
-	int heightOfTriangle;
-	int count1 = 1;
-	printf("Enter the height of the triangle: ");
-	scanf("%d", &heightOfTriangle);
-	while (count1<=heightOfTriangle) {
-	int count2 = 1;
-	while (count2<=heightOfTriangle) {
-		printf("*");
-		count2++;
-	}
-	printf("\n");
-	count1++;
-	}
+	int heightOfrectangle; // variable telling the height of triangle
+	int count1 = 1; // counter 1
+	printf("Enter the height of the rectangle: ");
+	scanf("%d", &heightOfrectangle);
+	
+	while (count1<=heightOfrectangle) {
+		int count2 = 1;
+		while (count2<=heightOfrectangle) {
+			printf("*");
+			count2++;
+		} // while loop ends 
+		printf("\n");
+		count1++;
+	} // while loop ends 
+	return 0;
 } // end main
