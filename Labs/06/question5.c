941af98d3a52ed1b573ffe47eda7382a59a41924
Havish-Chowdhry <142867564+Havish-Chowdhry@users.noreply.github.com>
2023-10-09 18:34:50 +0500

    Update question5.c

--- Labs/06/question5.c
+++ Labs/06/question5.c
@@ -1,5 +1,25 @@
 /*
 Programmer: Havish Chowdhry
-Desc: 
-Date: 08/10/2023
+Desc: Write a C program that produces the following output:
+0 0 0 0
+  1 1
+2 2 2 2
+  3 3
+4 4 4 4
+  5 5
+6 6 6 6
+Date: 09/10/2023
 */
+
+#include <stdio.h>
+
+int main ()
+{	
+	printf("\t\t\t\t\t\t 0 0 0 0\n");
+	printf("\t\t\t\t\t\t   1 1\n");
+	printf("\t\t\t\t\t\t 2 2 2 2\n");
+	printf("\t\t\t\t\t\t   3 3\n");
+	printf("\t\t\t\t\t\t 4 4 4 4\n"); 
+	printf("\t\t\t\t\t\t   5 5\n");
+	printf("\t\t\t\t\t\t 6 6 6 6\n");
+}
