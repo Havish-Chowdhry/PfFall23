046cb89bdf656cfa4ac46e40695ddcd074fccf35
Havish-Chowdhry <142867564+Havish-Chowdhry@users.noreply.github.com>
2023-10-15 02:39:09 +0500

    Update question6.c

--- Labs/07/question6.c
+++ Labs/07/question6.c
@@ -9,7 +9,7 @@ Description: Counting the frequency of elemnts in the array
 int main ()
 {
 	int index, n, frequency;; // n being the number of elements in the array; 
-	int element, innerIndex;  
+	int innerIndex;  
 	
 	printf("Enter the number of elements: ");
 	scanf("%d", &n);
@@ -17,40 +17,47 @@ int main ()
 	// initialising array with n elements where n would be input by the user
 	int myArray[n];
 	
+	// loop to store all elements in the array 
 	for (index = 0; index < n; index++)
 	{
 		printf("Enter a number: ");
 		scanf("%d", &myArray[index]);
 	}
 	
+	// loop to print the array 
 	for (index = 0; index< n; index ++ )
 	{
 		printf("%d ", myArray[index]);
 	}
 	
-	int visited[n];
+	int visited[n]; // initalising an array that will keep track of duplicates or same number that has already been visited.
+	// the array is initialised with variable length 
 
-    // Initialize visited array with zeros
-    for (index = 0; index < n; index++) 
+	// Initialize visited array with zeros
+    	for (index = 0; index < n; index++) 
 	{
-        visited[index] = 0;
-    }
-
+        	visited[index] = 0;
+	}
+	
+	// loop that does the counting of the frequency. This loop is runned through each element in the array.
 	for (index = 0; index < n; index++) 
 	{
-    	if (visited[index] == 1) 
+    		if (visited[index] == 1) 
 		{
-        	continue; // Skip already visited elements
+        	     continue; // Skip already visited elements
 		}
 	
-		frequency=1;
-	
-		for (innerIndex = index + 1; innerIndex< n; innerIndex++)
+		frequency=1; // resets to 1 after every iteration.
+		
+		// this loop compares the current element with every other element in the array
+		for (innerIndex = index + 1; innerIndex< n; innerIndex++) // inner index starts from one more than the outer index
 		{
-			if (myArray[index] == myArray[innerIndex]) 
+		
+			if (myArray[index] == myArray[innerIndex]) // comparison 
 			{
-				frequency = frequency + 1;
-				visited[innerIndex] = 1;
+		
+				frequency = frequency + 1; // when duplicates found, it increments the counter that is the frequency.
+				visited[innerIndex] = 1; // set to 1 that that this particular index is checked all duplicates have been counted.
 			}
 		}
 		printf("\n");
@@ -58,4 +65,6 @@ int main ()
 	}
 	
 	return 0;
+} // end main 	
+
 } // end main 
